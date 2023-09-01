6e3cc8397aac07b90e143562f85c1323c2c22f62
Nahuel Gil <nahuel.gil@woow.com.uy>
2023-09-01 14:21:36 -0300

    fix: :hammer: test fix

--- src/app/features/user/components/user-layout/user-layout.component.spec.ts
+++ src/app/features/user/components/user-layout/user-layout.component.spec.ts
@@ -1,11 +1,11 @@
 import { render } from '@testing-library/angular';
-import { OrganizationLayoutComponent } from './user-layout.component';
+import { UserLayoutComponent } from './user-layout.component';
 import { RouterModule } from '@angular/router';
 import { PageHeaderComponent } from '@shared/components/page-header/page-header.component';
 
 describe('OrganizationLayoutComponent', () => {
   it('should render OrganizationLayoutComponent', async () => {
-    const { container } = await render(OrganizationLayoutComponent, {
+    const { container } = await render(UserLayoutComponent, {
       declarations: [PageHeaderComponent],
       imports: [RouterModule]
     });
