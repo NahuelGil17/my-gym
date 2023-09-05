a6c8a18d7a9d761979650202e47f894eb37c16d2
Nahuel Gil <32676776+NahuelGil17@users.noreply.github.com>
2023-09-04 21:44:49 -0300

    feat: :sparkles: user table and user state (#12)
    Co-authored-by: Nahuel Gil <nahuel.gil@woow.com.uy>

---
+++ src/app/features/user/state/user.state.ts
@@ -0,0 +1,108 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user.interface';
+import { UserService } from '../services/user.service';
+import { catchError, tap } from 'rxjs/operators';
+import { Action, Selector, State, StateContext } from '@ngxs/store';
+import { DeleteUser, GetUser, GetUsers, UpdateUser } from './user.actions';
+import { throwError } from 'rxjs';
+
+export interface UsersStateModel {
+  users: User[];
+  loading: boolean;
+  error: HttpErrorResponse | null;
+}
+
+@State<UsersStateModel>({
+  name: 'users',
+  defaults: {
+    users: [],
+    loading: false,
+    error: null
+  }
+})
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersState {
+  constructor(private userService: UserService) {}
+
+  @Selector()
+  static getUsers(state: UsersStateModel): User[] {
+    return state.users;
+  }
+
+  @Selector()
+  static getUserById(state: UsersStateModel) {
+    return (id: string): User | undefined => {
+      return state.users.find((user) => user.id === id);
+    };
+  }
+
+  @Selector()
+  static isLoading(state: UsersStateModel): boolean {
+    return state.loading;
+  }
+
+  @Selector()
+  static getError(state: UsersStateModel): HttpErrorResponse | null {
+    return state.error;
+  }
+
+  @Action(GetUsers)
+  getUsers(ctx: StateContext<UsersStateModel>): void {
+    ctx.patchState({ loading: true, error: null });
+    this.userService.getUsers().pipe(
+      tap((users: User[]) => {
+        ctx.patchState({ users, loading: false });
+      }),
+      catchError((error) => {
+        ctx.patchState({ error, loading: false });
+        return throwError(() => error);
+      })
+    );
+  }
+
+  @Action(GetUser)
+  getUser(ctx: StateContext<UsersStateModel>, { id }: GetUser): void {
+    ctx.patchState({ loading: true, error: null });
+    this.userService.getUser(id).pipe(
+      tap((user: User) => {
+        ctx.patchState({ users: [user], loading: false });
+      }),
+      catchError((error) => {
+        ctx.patchState({ error, loading: false });
+        return throwError(() => error);
+      })
+    );
+  }
+  @Action(UpdateUser)
+  updateUser(ctx: StateContext<UsersStateModel>, { user }: UpdateUser): void {
+    ctx.patchState({ loading: true, error: null });
+    this.userService.updateUser(user).pipe(
+      tap((updatedUser: User) => {
+        const users = ctx.getState().users.map((u) => (u.id === updatedUser.id ? updatedUser : u));
+        ctx.patchState({ users, loading: false });
+      }),
+      catchError((error) => {
+        ctx.patchState({ error, loading: false });
+        return throwError(() => error);
+      })
+    );
+  }
+
+  // @Action(DeleteUser)
+  // deleteUser(ctx: StateContext<UsersStateModel>, { id }: DeleteUser): void {
+  //   ctx.patchState({ loading: true, error: null });
+  //   this.userService.deleteUser(id).pipe(
+  //     tap(() => {
+  //       const users = ctx.getState().users.filter((u) => u.id !== id);
+  //       ctx.patchState({ users, loading: false });
+  //     }),
+  //     catchError((error) => {
+  //       ctx.patchState({ error, loading: false });
+  //       return throwError(() => error);
+  //     })
+  //   );
+  // }
+}
