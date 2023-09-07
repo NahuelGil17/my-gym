1e44d27b136a0d629234a3e9d7deed8c17dc6c4a
Nico Gilardoni <nicogilardonik@gmail.com>
2023-09-07 20:53:17 -0300

    Changes

--- src/app/features/panel/components/clock/clock.component.ts
+++ src/app/features/panel/components/clock/clock.component.ts
@@ -7,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClockComponent implements OnInit {
   currentTime: string = new Date().toLocaleTimeString();
+  currentSecond: number = new Date().getSeconds();
+  currentHour: number = new Date().getHours();
 
   ngOnInit() {
     setInterval(() => {
-      this.currentTime = new Date().toLocaleTimeString();
+      const now = new Date();
+
+      if (now.getSeconds() !== this.currentSecond) {
+        this.currentSecond = now.getSeconds();
+        this.onSecondChange();
+      }
+
+      if (now.getHours() !== this.currentHour) {
+        this.currentHour = now.getHours();
+        this.onHourChange();
+      }
+
+      this.currentTime = now.toLocaleTimeString();
     }, 1000);
+  }
+
+  onSecondChange() {
+    console.log('Second has changed!');
+  }
+
+  onHourChange() {
+    //HACER PEDIDO PARA OBTENER LOS USUARIOS
   }
 }
