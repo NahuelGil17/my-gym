691196f5d384329fee3baa5b4e66033e7b243a4e
Nahuel Gil <nahuel.gil@woow.com.uy>
2023-09-21 21:13:54 -0300

    feat: :sparkles: panel remove and add panel

--- src/app/features/panel/state/panel.actions.ts
+++ src/app/features/panel/state/panel.actions.ts
@@ -4,3 +4,13 @@ export class GetPanels {
   static readonly type = '[Panel] Get Panels';
   constructor(public readonly payload: PanelPayload) {}
 }
+
+export class AddPanel {
+  static readonly type = '[Panel] Add Panel';
+  constructor(public readonly payload: any) {}
+}
+
+export class RemovePanel {
+  static readonly type = '[Panel] Remove Panel';
+  constructor(public readonly payload: any) {}
+}
